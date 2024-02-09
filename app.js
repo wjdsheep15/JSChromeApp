f80243e32e3ddeb58be7bb871f645b655a357761
wjdsheep15 <wjd15sheep@gmail.com>
2024-02-09 16:50:40 +0900

    learn Recap

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const player = {
-  name: "nico",
-  sayHello: function(otherPersonsName){
-    console.log("hello "+ otherPersonsName + " nice to meet you")
-  }
-}
+const  calculator= {
+  add: function(a,b){
+    console.log(a + b);
+  },
+  minus: function(a,b){
+    console.log(a -b);
+  },
+  div: function(a,b){
+    console.log(a/b);
+  },
+  multi: function(a,b){
+    console.log(a * b);
+  },
+  power: function(a,b){
+    console.log(a ** b);
+  },
+};
 
-console.log(player.name)
-player.sayHello("lynn");
+calculator.add(1,2)
+calculator.minus(1,2)
+calculator.div(1,2)
+calculator.multi(1,2)
+calculator.power(1,2)
