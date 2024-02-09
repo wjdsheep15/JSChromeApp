c7126df5a450c55611c3ff1c13deb9a1fb44d9ac
wjdsheep15 <wjd15sheep@gmail.com>
2024-02-09 17:49:24 +0900

    conditionals

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-const  calculator= {
-  add: function(a,b){
-    console.log(a + b);
-  },
-  minus: function(a,b){
-    console.log(a -b);
-  },
-  div: function(a,b){
-    console.log(a/b);
-  },
-  multi: function(a,b){
-    console.log(a * b);
-  },
-  power: function(a,b){
-    console.log(a ** b);
-  },
-};
+const age = parseInt(prompt("Hello old are you?"));
 
-calculator.add(1,2)
-calculator.minus(1,2)
-calculator.div(1,2)
-calculator.multi(1,2)
-calculator.power(1,2)
+
+if(isNaN(age) || age < 0){
+  console.log("Please write a number")
+}else if(age < 18){
+  console.log("You are too young.")
+}else if(age >= 18 && age <= 50){
+  console.log("You can drink")
+}else if(age > 50 && age <= 80){
+  console.log("You should exercise")
+}else if(age > 80) {
+  console.log("You can do whatever you want.")
+}
